d8e487a18305e321d9d8705b2c76321c4c7e4f3b
Jose Abreu <jose.abreu4@upr.edu>
2016-04-21 23:20:04 -0700

    User GUI created

---
+++ TakeHomeTest2/GUI.py
@@ -0,0 +1,40 @@
+from easygui import *
+import sys
+
+while 1:
+
+    msgbox("Welcome to Exam #2 Take Home. Please hit ok to enter the image")
+    imagePath = fileopenbox()
+    print(imagePath)
+
+    msg ="What image processing algorithm you want to  work with?"
+    title = "Image Processing Algorithms"
+    choices = ["MedianFilter", "WeinerFilter", "MarrHildrethEdgeDetector"]
+    choice = choicebox(msg, title, choices)
+    print(choice)
+    numberThreads = integerbox(msg="What is the number of threads you want to work with"
+                  , title = "Number Of Threads"
+                  , default = 0
+                  , lowerbound = 0
+                  , upperbound = 200)
+    print(numberThreads)
+    numberCores = integerbox(msg="What is the number of cores you want to work with"
+                  , title = "Number Of Cores"
+                  , default = 0
+                  , lowerbound = 0
+                  , upperbound = 300)
+    print(numberCores)
+    numberThreadsPerCore = integerbox(msg="What is the number of threads per core you want to work with"
+                  , title = "Number Of Threads per Core"
+                  , default = 0
+                  , lowerbound = 0
+                  , upperbound = 400)
+    print(numberThreadsPerCore)
+
+    msg = "Do you want to continue?"
+    title = "Please Confirm"
+    if ccbox(msg, title):     # show a Continue/Cancel dialog
+        pass  # user chose Continue
+    else:
+        sys.exit(0)
+
--- TakeHomeTest2/GUISample.py
+++
@@ -1,9 +0,0 @@
-"""import the 'tkinter' module
-"""
-import tkinter
-"""create a new window
-"""
-window = tkinter.Tk()
-"""draw the window, and start the 'application'
-"""
-window.mainloop()
